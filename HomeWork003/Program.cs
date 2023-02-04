0253810219427f6770ecdf3cb19ee26332b6a2c3
mibr2d <hodakov7419@gmail.com>
2023-02-04 11:29:32 +0300

    V1

---
+++ HomeWork003/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 3: Задайте массив вещественных чисел. Найдите 
+// разницу между максимальным и минимальным 
+// элементов массива.
+// [3, 7.4, 22.3, 2, 78] -> 76
+
+
+double[] GenerateArray(int len)
+{
+    double[] array = new double[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next();
+    }
+    return array;
+}
+double Maximum(double[] array)
+{
+    double maxValue = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (maxValue < array[i])
+        {
+            maxValue = array[i];
+
+        }
+
+    }
+    return maxValue;
+}
+double Minimum(double[] array)
+{
+    double minValue = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (minValue > array[i])
+        {
+            minValue = array[i];
+
+        }
+
+    }
+    return minValue;
+}
+
+void PrintArray(double[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+double[] array = GenerateArray(3);
+PrintArray(array);
+double maxValue = Maximum(array);
+double minValue = Minimum(array);
+double defference = maxValue - minValue;
+System.Console.WriteLine($"Максимальное число {maxValue}, Минимальное число {minValue}, Разность между максимальным и минимальным {defference}");
+
+
+
+
+
